5b838c590f417669d7a3ea4c9bf10eefaae1b02b
Abhinav Gupta <75429651+abhinavg2002@users.noreply.github.com>
2022-04-15 20:28:36 +0530

    Time: 735 ms (13.25%), Space: 208.3 MB (10.65%) - LeetHub

--- 1425-constrained-subsequence-sum/1425-constrained-subsequence-sum.cpp
+++ 1425-constrained-subsequence-sum/1425-constrained-subsequence-sum.cpp
@@ -3,22 +3,21 @@ public:
     int constrainedSubsetSum(vector<int>& v, int k) {
         int n=v.size();
         vector<int> dp(n, 0);
-        multiset<int> s;
-        int j=0;
+        multiset<int> s;// will contain our window and use islie kia coz hme max chaiye tha
+        int j=0;// window start
         for(int i=0;i<n;i++){
             dp[i]=v[i];
+            // agr window me kuch elements hai
             if(s.size()){
                 dp[i]+=max(0, *s.rbegin());
             }
+            // agr window ka size k ya usse bada hai to window ko km karo and = islie coz current element abhi add hoga islie km karo
             if(s.size()>=k){
                 s.erase(s.find(dp[j++]));
             }
+            // naya ans add kardo window me
             s.insert(dp[i]);
         }
-        // for(auto x:dp){
-        //     cout<<x<<" ";
-        // }
-        // cout<<endl;
         return *max_element(dp.begin(), dp.end());
     }
 };
