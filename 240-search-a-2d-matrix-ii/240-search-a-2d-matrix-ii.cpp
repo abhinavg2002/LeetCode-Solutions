af7e6cf7f0e6e90261fde624feae82bd23581f89
Abhinav Gupta <75429651+abhinavg2002@users.noreply.github.com>
2022-04-19 14:44:44 +0530

    Time: 188 ms (32.66%), Space: 14.9 MB (22.55%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& a, int x) {
-        vector<int> v;
         int n=a.size();
         int m=a[0].size();
-        for(int i=0;i<n;i++){
-            v.push_back(a[i][0]);
-        }
-        
-        int id=upper_bound(v.begin(), v.end(), x)-v.begin()-1;
-        if(id==-1)return false;
-        for(int i=0;i<=id;i++){
-            if(binary_search(a[i].begin(), a[i].end(), x)){
-                return true;
+        int i=0, j=m-1;
+        while(i<n and j>=0){
+            if(a[i][j]==x)return true;
+            if(a[i][j]<x){
+                i++;
+            }else{
+                j--;
             }
         }
         return false;
