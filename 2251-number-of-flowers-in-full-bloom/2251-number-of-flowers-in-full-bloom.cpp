f3d3b48fba8c58cf4b217a66d22036eabf80b93a
Abhinav Gupta <75429651+abhinavg2002@users.noreply.github.com>
2022-04-24 14:45:54 +0530

    Time: 376 ms (100.00%), Space: 89 MB (20.00%) - LeetHub

---
+++ 2251-number-of-flowers-in-full-bloom/2251-number-of-flowers-in-full-bloom.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> fullBloomFlowers(vector<vector<int>>& f, vector<int>& persons) {
+        // for ith person it can be like number of end after p[i]-number of start after p[i];
+        vector<int> ans;
+        vector<int> s, e;
+        for(auto x:f){
+            s.push_back(x[0]);
+            e.push_back(x[1]);
+        }
+        sort(s.begin(), s.end());
+        sort(e.begin(), e.end());
+        // for(auto x:s){
+        //     cout<<x<<" ";
+        // }
+        // cout<<endl;
+        // for(auto x:e){
+        //     cout<<x<<" ";
+        // }
+        // cout<<endl;
+        
+        for(auto x:persons){
+            auto it1=lower_bound(e.begin(), e.end(), x)-e.begin();
+            auto it2=upper_bound(s.begin(), s.end(), x)-s.begin();
+            // cout<<it1<<" "<<it2<<endl;
+            ans.push_back(it2-it1);
+        }
+        return ans;
+    }
+};
