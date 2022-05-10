045c93a7615f50811fec456f4bed5580f56b1944
Abhinav Gupta <75429651+abhinavg2002@users.noreply.github.com>
2022-05-10 11:50:50 +0530

    Time: 93 ms (37.62%), Space: 38.5 MB (63.13%) - LeetHub

--- 1375-number-of-times-binary-string-is-prefix-aligned/1375-number-of-times-binary-string-is-prefix-aligned.cpp
+++ 1375-number-of-times-binary-string-is-prefix-aligned/1375-number-of-times-binary-string-is-prefix-aligned.cpp
@@ -1,17 +1,12 @@
 class Solution {
 public:
-    int numTimesAllBlue(vector<int>& v) {
-        multiset<int> s, t;
-        vector<int> a=v;
-        int ct=0;
-        sort(a.begin(), a.end());
+    int numTimesAllBlue(vector<int>& a) {
+        int r=0;
         int n=a.size();
+        int ct=0;
         for(int i=0;i<n;i++){
-            s.insert(v[i]);
-            t.insert(a[i]);
-            if(s==t){
-                s.clear();
-                t.clear();
+            r=max(r, a[i]);
+            if(r==i+1){
                 ct++;
             }
         }
