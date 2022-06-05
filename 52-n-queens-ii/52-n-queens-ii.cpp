c25ea05eaf47e0b72335be74fb1c831d1cda641a
Abhinav Gupta <75429651+abhinavg2002@users.noreply.github.com>
2022-06-05 12:26:00 +0530

    Time: 8 ms (52.21%), Space: 6.1 MB (79.40%) - LeetHub

---
+++ 52-n-queens-ii/52-n-queens-ii.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    int ans=0;
+    bool is_safe_queen(vector<string> &grid, int row, int col, int n){
+        //vertically up
+        for(int i= row-1;i>=0;i--){
+            if(grid[i][col]=='Q')return false;
+        }
+        //horizontally left not needed
+        // for(int j=col-1;j>=0;j--){
+        //     if(grid[row][j]==true)return false;
+        // }
+        //upper left diagonal
+        for(int i=row-1, j=col-1;i>=0 && j>=0;i--,j--){
+            if(grid[i][j]=='Q')return false;
+        }
+        //lower right diag not needed
+        // for(int i=row+1, j=col+1;i<n && j<n;i++,j++){
+        //     if(grid[i][j]==true)return false;
+        // }
+        //upper right diag
+        for(int i=row-1, j=col+1;i>=0 && j<n;i--,j++){
+            if(grid[i][j]=='Q')return false;
+        }
+        //lower left diag not needed
+        // for(int i=row+1, j=col-1;i<n && j>=0;i++,j--){
+        //     if(grid[i][j]==true)return false;
+        // }
+        return true;
+    }
+    void n_queen(vector<string> &grid, int curr_row, int n){
+        //base case
+        if(curr_row==n){
+            ans++;
+            return ;
+        }
+        for(int i=0;i<n;i++){
+            if(is_safe_queen(grid,curr_row,i, n)){
+                grid[curr_row][i]='Q';
+                n_queen(grid,curr_row+1, n);
+                grid[curr_row][i]='.';//to make ques in original state
+            }
+        }
+    }
+    int totalNQueens(int n) {
+        vector<string> grid(n, string(n, '.'));
+        n_queen(grid, 0, n);
+        return ans;
+    }
+};
