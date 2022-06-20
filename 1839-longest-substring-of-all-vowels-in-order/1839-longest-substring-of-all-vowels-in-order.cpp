4d38eb1ca3a29e5702c7cfaf1e987fbf40796224
Abhinav Gupta <75429651+abhinavg2002@users.noreply.github.com>
2022-06-20 23:01:58 +0530

    Time: 154 ms (45.55%), Space: 26.8 MB (88.07%) - LeetHub

--- 1839-longest-substring-of-all-vowels-in-order/1839-longest-substring-of-all-vowels-in-order.cpp
+++ 1839-longest-substring-of-all-vowels-in-order/1839-longest-substring-of-all-vowels-in-order.cpp
@@ -1,31 +1,19 @@
 class Solution {
 public:
     int longestBeautifulSubstring(string s) {
-        vector<pair<char,int>> v;
         int n=s.size();
-        v.push_back({s[0], 1});
+        int l=1, ct=1, ans=0;
         for(int i=1;i<n;i++){
             if(s[i]==s[i-1]){
-                v.back().second++;
+                l++;
+            }else if(s[i-1]<s[i]){
+                ct++;l++;
             }else{
-                v.push_back({s[i], 1});
+                l=1, ct=1;
             }
-        }
-        // find a string aeiou
-        string t;
-        for(auto x:v){
-            t.push_back(x.first);
-        }
-        string f="aeiou";
-        auto it=t.find(f);
-        int ans=0;
-        while(it!=-1){
-            int l=0;
-            for(int i=0;i<5;i++){
-                l+=v[it+i].second;
+            if(ct==5){
+                ans=max(ans, l);
             }
-            ans=max(ans, l);
-            it=t.find(f, it+1);
         }
         return ans;
     }
