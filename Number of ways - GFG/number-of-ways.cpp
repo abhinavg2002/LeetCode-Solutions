0914138f9bd7f4a5e20f1c4f773b3d1ffcdc075d
Abhinav Gupta <75429651+abhinavg2002@users.noreply.github.com>
2022-06-30 00:16:06 +0530

    Added solution - LeetHub

---
+++ Number of ways - GFG/number-of-ways.cpp
@@ -0,0 +1,36 @@
+// { Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    long long int arrangeTiles(int n){
+        // code here
+        long long dp[n+1];
+        dp[0]=dp[1]=dp[2]=dp[3]=1;
+        for(int i=4;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-4];
+        }
+        return dp[n];
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        
+        Solution ob;
+        cout<<ob.arrangeTiles(N)<<"\n";
+    }
+    return 0;
+}  // } Driver Code Ends
