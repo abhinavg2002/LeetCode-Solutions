599ae097bb1211260aed15e55de52d0429fdc3a9
Abhinav Gupta <75429651+abhinavg2002@users.noreply.github.com>
2022-06-30 17:04:35 +0530

    Added solution - LeetHub

--- Get Minimum Squares - GFG/get-minimum-squares.cpp
+++ Get Minimum Squares - GFG/get-minimum-squares.cpp
@@ -9,22 +9,18 @@ using namespace std;
 
 class Solution{
 	public:
-	int dp[10001];
-	int fun(int n){
-	    if(n==0)return 0;
-	    if(n==1)return 1;
-	    if(dp[n]!=-1)return dp[n];
-	    int ans=1e9;
-	    for(int i=1;i*i<=n;i++){
-	        ans=min(ans, 1+fun(n-i*i));
-	    }
-	    return dp[n]=ans;
-	}
 	int MinSquares(int n)
 	{
-	    memset(dp, -1, sizeof(dp));
-	    return fun(n);
-	    // Code here
+	    int dp[n+1];
+	    dp[0]=0;
+	    dp[1]=1;
+	    for(int i=2;i<=n;i++){
+	        dp[i]=1e9;
+	        for(int j=1;j*j<=i;j++){
+	            dp[i]=min(dp[i], 1+dp[i-j*j]);
+	        }
+	    }
+	    return dp[n];
 	}
 };
 
