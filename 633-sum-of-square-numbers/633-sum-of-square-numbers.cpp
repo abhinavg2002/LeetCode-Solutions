470da1999d97868ce57089dfa93e99918a746c5b
Abhinav Gupta <75429651+abhinavg2002@users.noreply.github.com>
2022-07-07 18:56:44 +0530

    Time: 9 ms (31.32%), Space: 6 MB (25.73%) - LeetHub

--- 633-sum-of-square-numbers/633-sum-of-square-numbers.cpp
+++ 633-sum-of-square-numbers/633-sum-of-square-numbers.cpp
@@ -1,9 +1,18 @@
 class Solution {
 public:
+    #define ll long long
     bool judgeSquareSum(int c) {
-        for(int i=0;i<=sqrt(c);i++){
-            double d=sqrt(c-i*i);
-            if(d==(int)d)return true;
+        ll s=sqrt(c);
+        ll l=0, r=s;
+        while(l<=r){
+            ll v=l*l+r*r;
+            if(v==c){
+                return true;
+            }else if(v>c){
+                r--;
+            }else{
+                l++;
+            }
         }
         return false;
     }
