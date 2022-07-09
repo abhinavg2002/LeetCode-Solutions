a6da60e84c46988430d9b799a817c78609675d92
Abhinav Gupta <75429651+abhinavg2002@users.noreply.github.com>
2022-07-09 17:07:17 +0530

    Time: 204 ms (88.08%), Space: 80.6 MB (76.55%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.cpp
+++ 1696-jump-game-vi/1696-jump-game-vi.cpp
@@ -2,16 +2,14 @@ class Solution {
 public:
     int maxResult(vector<int>& a, int k) {
         int n=a.size();
+        deque<int> q{0};
         vector<int> dp(n);
         dp[0]=a[0];
-        multiset<int> s;
-        s.insert(dp[0]);
         for(int i=1;i<n;i++){
-            if(i>k){
-                s.erase(s.find(dp[i-k-1]));
-            }
-            dp[i]=a[i]+*rbegin(s);
-            s.insert(dp[i]);
+            if(q.front()<i-k)q.pop_front();
+            dp[i]=a[i]+dp[q.front()];
+            while(q.size() and dp[i]>=dp[q.back()])q.pop_back();
+            q.push_back(i);
         }
         return dp[n-1];
     }
