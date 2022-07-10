def8fabeca715b9c1ac9704c9b91cafef76cb281
Abhinav Gupta <75429651+abhinavg2002@users.noreply.github.com>
2022-07-11 01:20:46 +0530

    Added solution - LeetHub

---
+++ Reaching the heights - GFG/reaching-the-heights.cpp
@@ -0,0 +1,54 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+vector<int> reaching_height(int n, int a[]) ;
+
+int main() {
+	int t;
+	cin >> t;
+	while (t-- > 0) {
+	    int n; 
+	    cin >> n;
+	    int a[n];
+	    for (int i = 0; i < n; i++) 
+	        cin >> a[i];
+	    vector<int> v;
+	    v = reaching_height(n,a);
+	
+	    if(v.size()==1 and v[0] == -1){
+	        cout << "Not Possible\n";
+	    }
+	    else{
+	    for(int i:v)
+	        cout << i << " ";
+	    cout << endl;}
+	    
+	}
+	return 0;
+}// } Driver Code Ends
+
+
+//User function Template for C++
+
+vector<int> reaching_height(int n, int a[]) {
+    // Complete the function
+    vector<int> v(n);
+    sort(a, a+n);
+    int k=0;
+    int i,j;
+    for(i=0, j=n-1;i<j;i++,j--){
+        v[k++]=a[j];
+        v[k++]=a[i];
+    }
+    if(i==j){
+        v[k]=a[i];
+    }
+    if(n>=2 and v[0]<=v[1]){
+        return {-1};
+    }
+    return v;
+}
