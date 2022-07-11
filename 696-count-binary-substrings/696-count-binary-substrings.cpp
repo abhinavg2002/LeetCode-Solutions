9017ca171deb7f1c549fabe542799af3f50451cc
Abhinav Gupta <75429651+abhinavg2002@users.noreply.github.com>
2022-07-11 23:43:54 +0530

    Time: 43 ms (17.46%), Space: 10.6 MB (46.96%) - LeetHub

--- 696-count-binary-substrings/696-count-binary-substrings.cpp
+++ 696-count-binary-substrings/696-count-binary-substrings.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     #define pb push_back
     int countBinarySubstrings(string s) {
-        vector<pair<char,int>> v;
-        int n=s.size();
-        v.pb({s[0],1});
+        int pc=0, cc=1, n=s.size(), ans=0;
         for(int i=1;i<n;i++){
-            if(s[i]==s[i-1])v.back().second++;
-            else v.pb({s[i],1});
+            if(s[i]!=s[i-1]){
+                ans+=min(pc, cc);
+                pc=cc;
+                cc=1;
+            }else{
+                cc++;
+            }
         }
-        int ans=0;
-        for(int i=0;i<v.size()-1;i++){
-            ans+=min(v[i].second, v[i+1].second);
-        }
+        ans+=min(pc, cc);
         return ans;
     }
 };
