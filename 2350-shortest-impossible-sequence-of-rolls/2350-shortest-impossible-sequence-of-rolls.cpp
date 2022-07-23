de3597fa591a3ebdd2b1ecc55f0488ac053125f1
Abhinav Gupta <75429651+abhinavg2002@users.noreply.github.com>
2022-07-23 22:28:37 +0530

    Time: 601 ms (100.00%), Space: 119.3 MB (100.00%) - LeetHub

--- 2350-shortest-impossible-sequence-of-rolls/2350-shortest-impossible-sequence-of-rolls.cpp
+++ 2350-shortest-impossible-sequence-of-rolls/2350-shortest-impossible-sequence-of-rolls.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     int shortestSequence(vector<int>& r, int k) {
-        map<int,int> d;
+        map<int,int> m;
         int ct=0, s=0;
         for(auto x:r){
-            if(d[x]==s){
-                d[x]++;
-                ct++;
-                if(ct%k==0){
-                    s++;
-                }
+            m[x]++;
+            if(m.size()==k){
+                m.clear();
+                s++;
             }
         }
         return s+1;
