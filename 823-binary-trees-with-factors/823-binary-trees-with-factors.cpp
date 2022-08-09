5916780182c3b64e6ff8a6b0173996a79fae1852
Abhinav Gupta <75429651+abhinavg2002@users.noreply.github.com>
2022-08-09 15:18:51 +0530

    Time: 194 ms (20.92%), Space: 16.7 MB (25.54%) - LeetHub

--- 823-binary-trees-with-factors/823-binary-trees-with-factors.cpp
+++ 823-binary-trees-with-factors/823-binary-trees-with-factors.cpp
@@ -3,33 +3,19 @@ public:
     #define ll long long
     int mod=1e9+7;
     int n;
-    map<ll,multiset<ll>> m;
     map<ll,ll> dp;
-    ll fun(int root){
-        if(m.count(root)==0)return 1;
-        if(dp.count(root))return dp[root];
-        ll ans=1;
-        for(auto x:m[root]){
-            ans+=(1ll*fun(x)*fun(root/x));
-            ans%=mod;
-        }
-        return dp[root]=ans;
-    }
     int numFactoredBinaryTrees(vector<int>& a) {
         n=a.size();
-        unordered_set<int> s(a.begin(), a.end());
         sort(a.begin(), a.end());
+        ll ans=0;
         for(int i=0;i<n;i++){
+            dp[a[i]]=1;
             for(int j=0;j<i;j++){
-                if(a[i]%a[j]==0 and s.count(a[i]/a[j])){
-                    m[a[i]].insert(a[j]);
+                if(a[i]%a[j]==0){
+                    (dp[a[i]]+=(1ll*dp[a[j]]*dp[a[i]/a[j]]))%=mod;
                 }
             }
-        }
-        ll ans=0;
-        for(auto x:a){
-            ans+=fun(x);
-            ans%=mod;
+            (ans+=dp[a[i]])%=mod;
         }
         return ans;
     }
