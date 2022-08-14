b3edc5e623b75cc49a09451f1a355b140178d00a
Abhinav Gupta <75429651+abhinavg2002@users.noreply.github.com>
2022-08-14 14:25:02 +0530

    Time: 51 ms (26.62%), Space: 9.5 MB (72.68%) - LeetHub

--- 126-word-ladder-ii/126-word-ladder-ii.cpp
+++ 126-word-ladder-ii/126-word-ladder-ii.cpp
@@ -1,41 +1,80 @@
 class Solution {
 public:
-    vector<vector<string>> findLadders(string bw, string ew, vector<string>& wl) {
-        unordered_map<string, int> m;
-        for(auto &x:wl){
-            m[x]=1e9;
-        }
-        m[bw]=0;
-        vector<vector<string>> ans;
-        queue<pair<string, vector<string>>>q;
-        q.push({bw, {bw}});
-        while(q.size()){
-            int s=q.size();
-            while(s--){
-                auto p=q.front();
-                q.pop();
-                string w=p.first;
-                auto v=p.second;
-                if(w==ew){
-                    ans.push_back(v);
-                    continue;
+    bool able(string s,string t){
+        if(s.length()!=t.length())
+            return false;
+        int c=0;
+        for(int i=0;i<s.length();i++)
+            c+=(s[i]!=t[i]);
+        return c==1;
+    }
+    void bfs(vector<vector<int>> &g,vector<int> parent[],int n,int sr,int ds){
+        vector <int> dist(n,1005);
+        queue <int> q;
+        q.push(sr);
+        parent[sr]={-1};
+        dist[sr]=0;
+        while(!q.empty()){
+            int x=q.front();
+            q.pop();
+            for(int u:g[x]){
+                if(dist[u]>dist[x]+1){
+                    dist[u]=dist[x]+1;
+                    q.push(u);
+                    parent[u].clear();
+                    parent[u].push_back(x);
                 }
-                for(auto &x:w){
-                    char o=x;
-                    for(char c='a';c<='z';c++){
-                        x=c;
-                        if(x==o or w==bw or m.count(w)==0 or m[w]<(int)v.size()){
-                            continue;
-                        }
-                        m[w]=v.size();
-                        v.push_back(w);
-                        q.push({w, v});
-                        v.pop_back();
-                    }
-                    x=o;
-                }
+                else if(dist[u]==dist[x]+1)
+                    parent[u].push_back(x);
             }
         }
-        return ans;
     }
-};
+    void shortestPaths(vector<vector<int>> &Paths, vector<int> &path, vector<int> parent[],int node){
+        if(node==-1){
+            Paths.push_back(path);
+            return ;
+        }
+        for(auto u:parent[node]){
+            path.push_back(u);
+            shortestPaths(Paths,path,parent,u);
+            path.pop_back();
+        }
+    }
+    vector<vector<string>> findLadders(string beginWord, string endWord, vector<string>& wordList) {
+        int n=wordList.size(),sr=-1,ds=-1;
+        vector<vector<string>> ANS;
+        for(int i=0;i<n;i++){
+            if(wordList[i]==beginWord)
+                sr=i;
+            if(wordList[i]==endWord)
+                ds=i;
+        }
+        if(ds==-1)
+            return ANS;
+        if(sr==-1){
+            wordList.emplace(wordList.begin(),beginWord);
+            sr=0;
+            ds++;
+            n++;
+        }
+        vector <vector<int>> g(n,vector<int>()),Paths;
+        vector <int> parent[n],path;
+        for(int i=0;i<n-1;i++)
+            for(int j=i+1;j<n;j++)
+                if(able(wordList[i],wordList[j])){
+                    g[i].push_back(j);
+                    g[j].push_back(i);
+                }
+        bfs(g,parent,n,sr,ds); 
+        shortestPaths(Paths,path,parent,ds);
+        for(auto u:Paths){
+            vector <string> now;
+            for(int i=0;i<u.size()-1;i++)
+                now.push_back(wordList[u[i]]);
+            reverse(now.begin(),now.end());
+            now.push_back(wordList[ds]);
+            ANS.push_back(now);
+        }
+        return ANS;
+    }
+}; 
