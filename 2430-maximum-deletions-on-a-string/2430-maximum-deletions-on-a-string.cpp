853d7b2971c72cedeacaa8c307bf65746c5a8d17
Abhinav Gupta <75429651+abhinavg2002@users.noreply.github.com>
2022-10-12 15:59:57 +0530

    Time: 1457 ms (30.16%), Space: 338 MB (14.58%) - LeetHub

---
+++ 2430-maximum-deletions-on-a-string/2430-maximum-deletions-on-a-string.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    map<string,int> dp;
+    vector<int> solve(string s) {
+        int n = (int)s.length();
+        vector<int> pi(n);
+        for (int i = 1; i < n; i++) {
+            int j = pi[i-1];
+            while (j > 0 && s[i] != s[j])
+                j = pi[j-1];
+            if (s[i] == s[j])
+                j++;
+            pi[i] = j;
+        }
+        return pi;
+    }
+    int fun(string &s){
+        int n=s.size();
+        if(n==0)return 0;
+        if(dp.count(s))return dp[s];
+        auto a=solve(s);
+        int ans=1;
+        for(int i=0;i<n;i++){
+            if(2*a[i]==(i+1)){
+                string t=s.substr(a[i]);
+                ans=max(ans, 1+fun(t));
+            }
+        }
+        return dp[s]=ans;
+    }
+    int deleteString(string s) {
+        return fun(s);
+    }
+};
