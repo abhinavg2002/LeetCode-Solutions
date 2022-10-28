e498e9effeff60e1da7f94fd34cd6915863ec73d
Abhinav Gupta <75429651+abhinavg2002@users.noreply.github.com>
2022-10-28 15:31:26 +0530

    Time: 130 ms (94.54%), Space: 38.6 MB (88.46%) - LeetHub

--- 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.cpp
+++ 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.cpp
@@ -11,18 +11,17 @@ public:
             return ans;
         };
         
-        ll l=1, r=1e6;
-        while(r-l>2){
-            // cout<<l<<" "<<r<<endl;
-            ll m1 = l + (r - l) / 3;
-            ll m2 = r - (r - l) / 3;
-            ll f1 = f(m1);      //evaluates the function at m1
-            ll f2 = f(m2);      //evaluates the function at m2
-            if (f1 < f2)
-                r = m2;
-            else
-                l = m1;
+        ll l=1, r=1e6, ans=1e18;
+        while(l<r){
+            ll m=l+(r-l)/2;
+            ll f1=f(m), f2=f(m+1);
+            ans=min({ans, f1, f2});
+            if(f1<f2){
+                r=m;
+            }else{
+                l=m+1;
+            }
         }
-        return min({f(l), f(r), f(l+1)});
+        return ans;
     }
 };
