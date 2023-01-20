307d0546bcc9fc9b965ba552451c11cef8a229a2
Abhinav Gupta <75429651+abhinavg2002@users.noreply.github.com>
2023-01-20 16:44:09 +0530

    Added solution - LeetHub

---
+++ Maximum Weight Node - GFG/maximum-weight-node.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+//Initial Template for C++
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution
+{
+  public:
+  int maxWeightCell(int n, vector<int>e)
+  {
+      vector<int> v(n, 0);
+      for(int i=0;i<n;i++){
+          if(e[i]==-1)continue;
+          v[e[i]]+=i;
+      }
+      return max_element(v.begin(), v.end())-v.begin();
+  }
+};
+
+//{ Driver Code Starts.
+int main(){
+   int tc;
+   cin >> tc;
+   while(tc--){
+      int N;
+      cin >> N;
+      vector<int> Edge(N);
+      for(int i=0;i<N;i++){
+        cin>>Edge[i];
+      }
+      Solution obj;
+      int ans=obj.maxWeightCell(N, Edge);
+      cout<<ans<<endl;
+   }
+   return 0;
+}
+// } Driver Code Ends
